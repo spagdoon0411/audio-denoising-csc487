6cfb8928dad2867c9433c6d6e1d139acfdfd7967
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-03-11 16:52:48 -0700

    ensure data_config settings are used in unet_train.py

--- unet_train.py
+++ unet_train.py
@@ -2,13 +2,13 @@ from models.unet.unet import OurUNet
 from models.unet.test_unet_spec import model_spec
 import tensorflow as tf
 from utils.spectrogram_utils import SpectUtils
-from data_paths import data_paths 
+from data_paths import data_paths, data_config
 
 # Obtain references to spectrogram datasets
 train_data = tf.data.Dataset.load(data_paths["spectrograms"]["train"])
 test_data = tf.data.Dataset.load(data_paths["spectrograms"]["test"])
 
-spectutils = SpectUtils(sampling_rate=sample_rate, hop_length=hop_length)
+spectutils = SpectUtils(sampling_rate=data_config["sample_rate"], hop_length=data_config["hop_length"])
 
 test_vecs = tf.data.Dataset.load(data_paths["vectors"]["test"])
 train_vecs = tf.data.Dataset.load(data_paths["vectors"]["train"])
