7ead8c053d29f57e2eecf1fb4b2c7e051badef11
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-03-11 16:55:30 -0700

    use batch size of one; concat layers cannot append into single matrix

--- unet_train.py
+++ unet_train.py
@@ -17,11 +17,11 @@ for mixedvec, cleanvec in train_vecs.take(1):
     spectutils.save_numpy_as_wav(mixedvec, "./example_mixed.wav")
     spectutils.save_numpy_as_wav(cleanvec, "./example_clean.wav")
 
-test_data=test_data.batch(5)
-train_data=train_data.batch(5)
+test_data=test_data.batch(1)
+train_data=train_data.batch(1)
 
 unetbuilder = OurUNet()
 unet = unetbuilder.build_model(modelspec=model_spec)
 unet.compile(optimizer='adam', loss="mse", metrics=["accuracy"])
-unet.fit(train_data, validation_data=test_data, epochs=1, batch_size=5, shuffle=True)
+unet.fit(train_data, validation_data=test_data, epochs=1, batch_size=1, shuffle=True)
 
