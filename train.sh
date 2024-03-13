581685b61f9f24020a8e9480912f8f1c3c59797b
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-03-12 19:11:29 -0700

    add early stopping, history saving, and cl-args to unet training scripts

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-python unet_train.py
+# This Python script has three command line args: the model history path, the model checkpoint path, and the number of epochs
+python unet_train.py $1 $2 $3
--- unet_train.py
+++ unet_train.py
@@ -6,12 +6,33 @@ from models.unet.unet import OurUNet
 from data_paths import data_config
 from utils.spectrogram_utils import SpectUtils
 from keras.api._v2.keras.utils import plot_model
+import sys
+import pickle
 
 
 spectutils = SpectUtils(sampling_rate=data_config["sample_rate"],
                         hop_length=data_config["hop_length"],
                         frame_length=data_config["frame_length"],
                         fft_length=data_config["fft_length"])
+
+
+earlystop = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    min_delta=0,
+    patience=1,
+    verbose=1,
+    mode="auto",
+    baseline=None,
+    restore_best_weights=False,
+    start_from_epoch=0,
+)
+
+history = tf.keras.callbacks.History()
+
+model_path = sys.argv[2]
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=model_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 # Obtain references to spectrogram datasets
 train_data = tf.data.Dataset.load(data_paths["spectrograms"]["train"])
@@ -28,7 +49,12 @@ for mixedvec, cleanvec in train_vecs.take(1):
 test_data = test_data.batch(1)
 train_data = train_data.batch(1)
 
+epochs = int(sys.argv[3])
 unetbuilder = OurUNet()
 unet = unetbuilder.build_model(modelspec=model_spec)
-unet.compile(optimizer="adam", loss="mse", metrics=["accuracy", "mae"])
-unet.fit(train_data, validation_data=test_data, epochs=1, batch_size=1, shuffle=True)
+unet.compile(optimizer="adam", loss="mse", metrics=["mse", "mae", "val_mse", "val_mae"])
+unet.fit(train_data, validation_data=test_data, epochs=epochs, batch_size=1, shuffle=True, callbacks=[earlystop, history, cp_callback])
+
+history_path = sys.argv[1]
+with open(history_path, 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
