68b0bfb48411a70280a951abf8720744c8f11660
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-03-15 07:18:27 -0700

    make docker_start.sh runnable from root dir

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,3 +1,3 @@
-bash import.sh
-bash process_data.sh
+bash docker/import.sh
+bash docker/process_data.sh
 python amp_train.py ./research_notes/histories/amp_train ./research_notes/models/amp_train $1
