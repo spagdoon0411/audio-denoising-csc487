64def1ffa0b12a7e52adf50712c8072f01c27c49
spagdoon0411 <spandan.suthar@live.com>
2024-03-23 03:48:07 -0700

    cleaned dir

---
+++ archive/unet_spectrogram_test.py
@@ -0,0 +1,5 @@
+from models.unet.unet import OurUNet
+from utils.dataset_utils import AudioData
+
+clean_audio_path
+data : AudioData = AudioData(clean_audio_path=)
--- unet_train.py
+++ unet_train.py
@@ -28,7 +28,7 @@ earlystop = tf.keras.callbacks.EarlyStopping(
 
 history = tf.keras.callbacks.History()
 
-model_path = sys.argv[2]
+model_path = sys.argv[1]
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=model_path,
                                                  save_weights_only=True,
                                                  verbose=1)
@@ -41,19 +41,19 @@ test_data = tf.data.Dataset.load(data_paths["spectrograms"]["test"]).prefetch(tf
 test_vecs = tf.data.Dataset.load(data_paths["vectors"]["test"])
 train_vecs = tf.data.Dataset.load(data_paths["vectors"]["train"])
 
-for mixedvec, cleanvec in train_vecs.take(1):
-    spectutils.save_numpy_as_wav(mixedvec, "./example_mixed.wav")
-    spectutils.save_numpy_as_wav(cleanvec, "./example_clean.wav")
+# for mixedvec, cleanvec in train_vecs.take(1):
+#    spectutils.save_numpy_as_wav(mixedvec, "./example_mixed.wav")
+#    spectutils.save_numpy_as_wav(cleanvec, "./example_clean.wav")
 
 test_data = test_data.batch(1)
 train_data = train_data.batch(1)
 
-epochs = int(sys.argv[3])
+epochs = int(sys.argv[2])
 unetbuilder = OurUNet()
 unet = unetbuilder.build_model(modelspec=model_spec)
 unet.compile(optimizer="adam", loss="mse", metrics=["mse", "mae"])
 unet.fit(train_data, validation_data=test_data, epochs=1, batch_size=1, shuffle=True, callbacks=[earlystop, history, cp_callback])
 
-history_path = sys.argv[1]
-with open(history_path, 'wb') as file_pi:
-    pickle.dump(history.history, file_pi)
+# history_path = sys.argv[1]
+# with open(history_path, 'wb') as file_pi:
+#     pickle.dump(history.history, file_pi)
