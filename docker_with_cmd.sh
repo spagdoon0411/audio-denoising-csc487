6262e40fd700dca4e1b78c69cd0c45648db8a4d6
spagdoon0411 <spandan.suthar@live.com>
2024-03-23 17:43:47 -0700

    clear old temp files

--- docker_with_cmd.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --gpus all --rm -it -p 8888:8888 -v $PWD:/tf -w /tf tensorflow/tensorflow:latest-gpu $1
--- unet_spectrogram_test.py
+++
@@ -1,5 +0,0 @@
-from models.unet.unet import OurUNet
-from utils.dataset_utils import AudioData
-
-clean_audio_path
-data : AudioData = AudioData(clean_audio_path=)
